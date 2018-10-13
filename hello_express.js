8aff0e9491702ecf038eb182cd95275d4266e599
lizizhen <lizizhen@cisystemsolutions.com>
2018-10-13 18:41:18 +0800

    20181013

--- hello_express.js
+++ hello_express.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 app.listen(3000, () => {
-  console.log('on port 3000');
+  // console.log('on port 3000');
 });
 //# sourceMappingURL=hello_express.js.map
